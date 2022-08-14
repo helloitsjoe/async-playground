838f51ca0b0e5f72da45da0da102948548a46439
helloitsjoe <jboyle617@gmail.com>
2022-08-14 00:20:20 -0400

    simplify date string

--- server/utils.js
+++ server/utils.js
@@ -1,9 +1,11 @@
 const startPerf = (id) => {
+  const simplifiedDateString = new Date()
+    .toISOString()
+    .replace('Z', '')
+    .split('T')[1];
+
   const start = Date.now();
-  console.log(
-    `${id} start:`,
-    new Date().toISOString().replace('Z', '').split('T')[1]
-  );
+  console.log(`${id} start:`, simplifiedDateString);
 
   return () => {
     console.log(`Took ${id} ${Date.now() - start} ms`);
